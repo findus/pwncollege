7417d6a47e13593f59b85bf7a9f906efcced3f79
Philipp Hentschel <philipp@f1ndus.de>
2023-06-28 11:04:33 +0200

    cleanup

--- program_interaction/level29.py
+++ program_interaction/level29.py
@@ -8,18 +8,10 @@ print(res.stdout)
 print(res.stderr)
 
 s = ssh(host="pwn")
-f = SSHPath('/tmp/my_script.py', ssh=s)
-f.touch()
-f.write_text('import subprocess; subprocess.run(["/challenge/embryoio_level29"])')
-
-f2 = SSHPath('/tmp/nrzqjx', ssh=s)
-f2.touch()
-f2.write_text('lgwuuobv')
 
 s.process(["gcc",'-o', '/home/hacker/pwncollege', "/tmp/pwncollege.c"])
 
 p = s.process(['/home/hacker/pwncollege'])
-#p.sendline('dremewyh')
 r = p.clean(timeout=2)
 res=repr(r)
 print(res)
