95385d5364230c85c0bdc661895ee9d76121f992
Philipp Hentschel <philipp@f1ndus.de>
2023-07-01 00:23:22 +0200

    l2

---
+++ assembly/level2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from pwn import *
+
+context.arch = 'amd64'
+#context.endian = 'little'
+
+ssh = ssh(host='pwn')
+p=ssh.process('/challenge/run')
+
+shellcode= asm('''
+               .intel_syntax noprefix
+               add  rdi,0x331337
+               ''',arch='amd64')
+
+print(disasm(shellcode, arch = 'amd64'))
+print(disasm(shellcode, arch = 'x86_64'))
+#print(bytes(shellcode))
+print('1', shellcode)
+shellcode=bytes(shellcode)
+#print('1', shellcode)
+p.send(shellcode)
+print(p.recvall(), end='\\n' )
